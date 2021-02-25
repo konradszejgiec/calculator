2ea10e74491b52c7a6912c990b45cc855f6f17c4
Konrad Szejgiec <67743014+konradszejgiec@users.noreply.github.com>
2021-02-25 09:29:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,139 +1,180 @@
 class Calculator {
-	constructor(numbers = [], numbers1 = [], sign = []) {
-		this.panel = document.querySelector(".panel");
-		this.result = document.querySelector(".result");
-		this.result.textContent = "0";
-		this.numbers = numbers;
-		this.numbers1 = numbers1;
-		this.sign = sign;
-		this.results = 0;
-		this.panel.addEventListener("click", this.addNumber.bind(this));
-		window.addEventListener("keydown", this.addNumber.bind(this));
-	}
+  constructor(
+    leftSideActionNumbers = [],
+    rightSideActionNumbers = [],
+    sign = [],
+    operators = {
+      math: ["+", "-", "/", "*"],
+      equal: ["=", "Enter"],
+      clear: ["C", "Delete", "Escape"],
+      dot: ["."]
+    },
+    results = 0
+  ) {
+    this.calcDiv = document.querySelector(".panel");
+    this.resultPanel = document.querySelector(".result");
+    this.resultPanel.textContent = "0";
+    this.leftSideActionNumbers = leftSideActionNumbers;
+    this.rightSideActionNumbers = rightSideActionNumbers;
+    this.sign = sign;
+    this.operators = operators;
+    this.results = results;
+    this.calcDiv.addEventListener(
+      "click",
+      this.executeMathOperation.bind(this)
+    );
+    window.addEventListener("keydown", this.executeMathOperation.bind(this));
+  }
 
-	addNumber(e) {
-		//checking result for infinity and NaN	
-		if (this.results === Infinity || isNaN(this.results)) {
-			this.results = 0;
-			if ((this.numbers[0] == "Infinity") || isNaN(this.numbers[0])) {
-				this.reset();
-			}
-		}
-		//checking type of event
-		this.event = e.type === "keydown" ? e.key : e.target.textContent;
-		//checking number 
-		if ((e.target.textContent === "+" || e.target.textContent === "-" || e.target.textContent === "/" || e.target.textContent === "*" || e.target.textContent === "." || e.target.textContent === "C" || e.target.textContent === "=") || (e.key === "+" || e.key === "-" || e.key === "/" || e.key === "*" || e.key === "." || e.key === "Delete" || e.key === "Escape" || e.key === "Enter")) {
-			this.number = this.event;
-		} else this.number = Number(this.event);
-		//checking target	
-		if (isNaN(this.number) && typeof (this.number) !== "string") return;
-		//checking equality flag 
-		if (this.equalityFlag === false) {
-			if (this.number === "+" || this.number === "-" || this.number === "/" || this.number === "*" || this.number === "Enter" || this.number === "Delete" || this.number === "Escape") {
-				this.numbers1 = [];
-				this.numbers = [this.results];
-				this.result.textContent = this.results;
-			} else this.reset()
-			this.equalityFlag = true;
-		}
-		//checking result 
-		if (this.numbers.length === 0 && this.numbers1.length === 0 && this.sign.length === 0 && this.results === 0) {
-			this.result.textContent = "";
-		}
-		//pushing to array and result panel 
-		if (this.result.textContent === "0" && this.numbers.length === 0) {
-			this.result.textContent = this.number;
-		} else this.result.textContent += this.number
-		this.pushToArray();
-		//checking operation 
-		if (this.sign.length > 1) {
-			if (this.numbers1.length === 0) {
-				this.sign = [this.number];
-				this.result.textContent = this.numbers.join("") + this.number;
-			} else {
-				this.equalality();
-				this.numbers = [this.results];
-				this.numbers1 = [];
-				this.pushToArray();
-				this.sign = [];
-				this.sign = [this.event];
-				this.result.textContent = (!isFinite(this.results)) ? "Nie dziel przez 0" : this.results + this.sign;
-			}
-		}
-		//reset 
-		if (this.number === "C" || this.number === "Delete" || this.number === "Escape") {
-			this.reset();
-			this.results = 0;
-		}
-		//equality 
-		if (this.number === "=" || this.number === "Enter") {
-			if (this.numbers.length > 0 && this.numbers1.length === 0 && this.sign.length === 0) {
-				this.results = this.numbers.join("");
+  executeMathOperation(e) {
+    //checking result for infinity and NaN
+    if (this.results === Infinity || isNaN(this.results)) {
+      this.results = 0;
+      if (
+        this.leftSideActionNumbers[0] == "Infinity" ||
+        isNaN(this.leftSideActionNumbers[0])
+      ) {
+        this.reset();
+      }
+    }
+    //checking type of event
+    this.event = e.type === "keydown" ? e.key : e.target.textContent;
+    //checking for string || number
+    this.number =
+      this.checkOperators(this.event, this.operators.math.concat(this.operators.clear, this.operators.equal, this.operators.dot)) ?
+      this.event :
+      Number(this.event);
+    //checking target
+    if (isNaN(this.number) && typeof this.number !== "string") return;
+    //checking equality flag
+    if (this.equalityFlag === false) {
+      if (
+        this.checkOperators(this.number, this.operators.math.concat(this.operators.equal, this.operators.clear))
+      ) {
+        this.rightSideActionNumbers = [];
+        this.leftSideActionNumbers = [this.results];
+        this.resultPanel.textContent = this.results;
+      } else this.reset();
+      this.equalityFlag = true;
+    }
+    //checking result
+    if (
+      this.leftSideActionNumbers.length === 0 &&
+      this.rightSideActionNumbers.length === 0 &&
+      this.sign.length === 0 &&
+      this.results === 0
+    ) {
+      this.resultPanel.textContent = "";
+    }
+    //pushing to array and result panel
+    this.resultPanel.textContent =
+      this.resultPanel.textContent === "0" &&
+      this.leftSideActionNumbers.length === 0 ?
+      this.number :
+      this.resultPanel.textContent + this.number;
+    this.pushToArray();
+    //checking for operation
+    if (this.sign.length > 1) {
+      if (this.rightSideActionNumbers.length === 0) {
+        this.sign = [this.number];
+        this.resultPanel.textContent =
+          this.leftSideActionNumbers.join("") + this.number;
+      } else {
+        this.calc(this.equalityFlag, this.leftSideActionNumbers, this.rightSideActionNumbers, this.sign)
+        this.leftSideActionNumbers = [this.results];
+        this.rightSideActionNumbers = [];
+        this.pushToArray();
+        this.sign = [];
+        this.sign = [this.event];
+        this.resultPanel.textContent = this.checkingNanAndInfinity(this.results) ?
+          "Nie dziel przez 0" :
+          this.results + this.sign;
+      }
+    }
+    //reset
+    if (this.checkOperators(this.number, this.operators.clear)) {
+      this.reset();
+      this.results = 0;
+    }
+    //equality
+    if (this.checkOperators(this.number, this.operators.equal)) {
+      this.results =
+        this.leftSideActionNumbers.length > 0 &&
+        this.rightSideActionNumbers.length === 0 &&
+        this.sign.length === 0 ?
+        this.leftSideActionNumbers.join("") :
+        Number(this.results).toFixed();
 
-				if (this.results >= 0) {
-					this.results = Number(this.results).toFixed();
-				}
-			}
+      if (this.rightSideActionNumbers.length === 0) {
+        this.rightSideActionNumbers[0] =
+          this.sign.includes("+") || this.sign.includes("-") ? [] : 1;
+      }
 
-			if (this.numbers1.length === 0) {
-				this.numbers1[0] = (this.sign[0] == "+" || this.sign[0] == "-") ? [] : 1;
-			}
+      this.calc(this.equalityFlag, this.leftSideActionNumbers, this.rightSideActionNumbers, this.sign)
+      this.reset();
+      this.resultPanel.textContent = this.checkingNanAndInfinity(this.results) ?
+        "Nie dziel przez 0" :
+        this.results;
+      this.leftSideActionNumbers = [this.results];
+      this.equalityFlag = false;
+    }
+  }
 
-			this.equalality();
-			this.reset();
-			this.result.textContent = (!isFinite(this.results) || isNaN(this.results)) ? "Nie dziel przez 0" : this.results;
-			this.numbers = [this.results];
-			this.equalityFlag = false;
-		}
-		//console.log(this.numbers);
-		//console.log(this.numbers1);
-		//console.log(this.sign);
-		//console.log(this.results);
-	}
+  pushToArray() {
+    if (
+      this.sign.length === 0 &&
+      !this.checkOperators(this.number, this.operators.math.concat(this.operators.equal))
+    ) {
+      this.leftSideActionNumbers.push(this.number);
+    }
+    if (
+      this.sign.length === 1 &&
+      !this.checkOperators(this.number, this.operators.math.concat(this.operators.equal))
+    ) {
+      this.rightSideActionNumbers.push(this.number);
+    }
+    if (this.checkOperators(this.number, this.operators.math)) {
+      this.sign.push(this.number);
+    }
+  }
 
-	pushToArray() {
-		if (this.sign.length === 0 && this.number !== "+" && this.number !== "-" && this.number !== "*" && this.number !== "/" && this.number !== "=" && this.number !== "Enter") {
-			this.numbers.push(this.number);
-		}
-		if (this.sign.length === 1 && this.number !== "+" && this.number !== "-" && this.number !== "*" && this.number !== "/" && this.number !== "=" && this.number !== "Enter") {
-			this.numbers1.push(this.number);
-		}
-		if (this.number === "+" || this.number === "-" || this.number === "*" || this.number === "/") {
-			this.sign.push(this.number);
-		}
-	}
+  calc(flag, lefSideNumbers, rightSideNumbers, sign) {
+    flag = flag
+    flag = true;
+    const numbersLeft = lefSideNumbers.join("") * 1;
+    const numbersRight = rightSideNumbers.join("") * 1;
+    this.results = sign.includes("*") ?
+      numbersLeft * numbersRight :
+      sign.includes("+") ?
+      numbersLeft + numbersRight :
+      sign.includes("-") ?
+      numbersLeft - numbersRight :
+      sign.includes("/") ?
+      numbersLeft / numbersRight :
+      this.number.length > 0 ?
+      lefSideNumbers.join("") :
+      0;
+  }
 
-	equalality() {
-		this.equalityFlag = true;
-		this.numbersOne = this.numbers.join("") * 1;
-		this.numbersTwo = this.numbers1.join("") * 1;
-		//console.log(numbers, typeof numbersOne); 
-		if (this.sign[0] === "*") {
-			this.results = this.numbersOne * this.numbersTwo;
-		}
-		if (this.sign[0] === "+") {
-			this.results = this.numbersOne + this.numbersTwo;
-		}
-		if (this.sign[0] === "-") {
-			this.results = this.numbersOne - this.numbersTwo;
-		}
-		if (this.sign[0] === "/") {
-			this.results = this.numbersOne / this.numbersTwo;
-		}
-	}
+  reset() {
+    this.resultPanel.textContent = "0";
+    this.leftSideActionNumbers = [];
+    this.rightSideActionNumbers = [];
+    this.sign = [];
+  }
 
-	reset() {
-		this.result.textContent = "0";
-		this.numbers = [];
-		this.numbers1 = [];
-		this.sign = [];
-	}
+  correct() {
+    if (this.event === "Backspace") {}
+  }
 
-	correct() {
-		if (this.event === "Backspace") {
+  checkOperators(text, operator) {
+    operator = operator;
+    return operator.includes(text);
+  }
 
-		}
-	}
+  checkingNanAndInfinity(variable) {
+    return !isFinite(variable) || isNaN(variable);
+  }
 }
 
 const newCalculator = new Calculator();
